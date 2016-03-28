4ec992a81fe02d2f9514fe4f1db7cdfbc2ae484d
a1ex7 <ta-lex@mail.ru>
2016-03-28 16:20:35 +0300

    task1 js7-8

--- js_07_08/task1/js/script.js
+++ js_07_08/task1/js/script.js
@@ -1,3 +1,18 @@
 $(function() {
-    $('body').css('background', 'red');
+
+    $('.tabs__text:not(.tabs__text--active)').hide();
+    var $tabsLink = $('.tabs__link');
+    
+    $tabsLink.on('click', function(e) {
+        e.preventDefault();
+
+        var tabId = $(this).attr('href');
+
+        $tabsLink.removeClass('tabs__link--active');
+        $(this).addClass('tabs__link--active');
+
+        $('.tabs__text').hide();
+        $(tabId).show();
+    });
+
 });
